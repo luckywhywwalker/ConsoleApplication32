020740a8cae998822fc7aba437df6192638db7a4
lucky <kherallakarimo8@gmail.com>
2024-11-25 23:48:42 +0200

    Update ConsoleApplication32.cpp

--- ConsoleApplication32/ConsoleApplication32.cpp
+++ ConsoleApplication32/ConsoleApplication32.cpp
@@ -1,12 +1,20 @@
-﻿#include <iostream>
+#include <iostream>
 #include <cstdio> 
 using namespace std;
 int main() {
+    FILE* ptrFile = fopen("file.txt", "w");
+    if (ptrFile != NULL) {
+        fputs("adafafafafafffafgggagfagag", ptrFile);
+        fclose(ptrFile);
+    } else {
+        cout << "Error: Could not create or open the file for writing." << endl;
+        return 1;
+    }
     int letterCount = 0; 
     char ch;
-    FILE* file = fopen("input.txt", "r");
+    FILE* file = fopen("file.txt", "r");
     if (file == nullptr) {
-        cout << "Error: Could not open the file." << endl;
+        perror("Error opening file");
         return 1;
     }
     while ((ch = fgetc(file)) != EOF) {
@@ -18,4 +26,3 @@ int main() {
     cout << "Number of letters in the file: " << letterCount << endl;
     return 0;
 }
-
