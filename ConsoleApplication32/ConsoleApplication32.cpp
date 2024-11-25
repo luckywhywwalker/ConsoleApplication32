a640c09c7f84d97b15b5a9bd006e6a0800f83396
Heralla Karim <kherallakarimo8@gmail.com.com>
2024-11-25 23:28:40 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApplication32/ConsoleApplication32.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <cstdio> 
+using namespace std;
+int main() {
+    int letterCount = 0; 
+    char ch;
+    FILE* file = fopen("input.txt", "r");
+    if (file == nullptr) {
+        cout << "Error: Could not open the file." << endl;
+        return 1;
+    }
+    while ((ch = fgetc(file)) != EOF) {
+        if ((ch >= 'A' && ch <= 'Z') || (ch >= 'a' && ch <= 'z')) {
+            letterCount++;
+        }
+    }
+    fclose(file);
+    cout << "Number of letters in the file: " << letterCount << endl;
+    return 0;
+}
+
